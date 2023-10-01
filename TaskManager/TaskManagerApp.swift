c2f64c0a3a56d56258532b71ac626de575194bc5
zhang-hongshen <zhanghongshen716@gmail.com>
2023-10-01 18:21:14 +0800

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by 张鸿燊 on 1/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
